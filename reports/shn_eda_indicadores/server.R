39acee936a31104ccb39dfa1d6683cb79ae60362
Songeo <sonia.gmcl@gmail.com>
2018-03-07 01:07:14 -0600

    nuevas tablas en shiny

--- reports/shn_eda_indicadores/server.R
+++ reports/shn_eda_indicadores/server.R
@@ -11,46 +11,165 @@ library(shiny)
 library(tidyverse)
 library(mxmaps)
 
-load("../../cache/tab_delitos_urec.RData")
+# load("../../cache/tab_delitos_urec.RData")
+load("../../cache/tab_union_indicadores.RData")
 theme_set(theme_minimal(base_size = 16))
+
+tab_delitos_urec <-  tab_union_indicadores %>% 
+  filter(Tema %in% c("víctima hogar", 
+                     "víctima persona", 
+                     "secretariado")) 
+
+tab_importancia <-  tab_union_indicadores %>% 
+  filter(Tema == "importancia") 
 
 shinyServer(function(input, output) {
    
-  TabGG <- reactive({
-    
+  
+  StateSelec <- reactive({
     if(input$estado.input == "all"){
       state_selec <- unique(tab_delitos_urec$state_code)  
     }
     if(input$estado.input != "all"){
       state_selec <- unique(input$estado.input)
     }
+    state_selec
+  })
+  
+  TabInds <- reactive({
+    state_selec <- StateSelec()
     
-    
+    indicador_selec <- input$indicador.input
+    if(input$indicador.input == "robo"){
+      indicador_selec <- c("robo", "robo calle", "robo otro")
+    }
     
     tab_inds <- tab_delitos_urec %>% 
-      filter(gpo_indicador == input$indicador.input)
+      filter(Descripción %in% indicador_selec ) 
     tab_gg <- mxmunicipio.map %>% 
       as.tibble() %>% 
       mutate(state_code = parse_number(str_sub(id, 1, 2)), 
              mun_code = parse_number(str_sub(id, 3, 5)) ) %>% 
-      left_join(tab_inds,
+      full_join(tab_inds,
                 by = c("state_code", "mun_code")) %>% 
-      filter(state_code %in% state_selec)
-    tab_gg
+      filter(state_code %in% state_selec) 
   })
   
-  output$map_gg <- renderPlot({
-    tab_gg <- TabGG() 
+  output$map_inds_gg <- renderPlot({
+    tab_gg <- TabInds() 
     tab_gg %>% 
       ggplot() +  
       geom_polygon(aes(x = long, 
                        y = lat,
                        group = group, 
-                       fill = value),  
+                       fill = valor),  
                    color = "white", 
                    size = .001) + 
-      facet_wrap(~indicador) + 
+      facet_wrap(~Tema + Descripción) + 
       theme(legend.position = "bottom") + 
+      guides(fill = guide_legend(direction = "vertical")) +
+      coord_equal()
+  })
+  
+  
+  output$map_importancia_gg <- renderPlot({
+    state_selec <- StateSelec()
+    
+    mxmunicipio.map %>% 
+      as.tibble() %>% 
+      mutate(state_code = parse_number(str_sub(id, 1, 2)), 
+             mun_code = parse_number(str_sub(id, 3, 5)) ) %>% 
+      full_join(tab_union_indicadores %>% 
+                  filter(Tema == "importancia") %>% 
+                  filter(Descripción == input$importancia.input),
+                by = c("state_code", "mun_code")) %>% 
+      filter(state_code %in% state_selec) %>% 
+      ggplot() +  
+      geom_polygon(aes(x = long, 
+                       y = lat,
+                       group = group, 
+                       fill = valor),  
+                   color = "white", 
+                   size = .001) + 
+      facet_wrap(~ Descripción) + 
+      theme(legend.position = "bottom") + 
+      guides(fill = guide_legend(direction = "vertical")) +
+      coord_equal()
+  })
+  
+  output$map_percepcion_gg <- renderPlot({
+    state_selec <- StateSelec()
+    
+    mxmunicipio.map %>% 
+      as.tibble() %>% 
+      mutate(state_code = parse_number(str_sub(id, 1, 2)), 
+             mun_code = parse_number(str_sub(id, 3, 5)) ) %>% 
+      full_join(tab_union_indicadores %>% 
+                  filter(Tema == "percepción") %>% 
+                  filter(Descripción == input$percepcion.input),
+                by = c("state_code", "mun_code")) %>% 
+      filter(state_code %in% state_selec) %>% 
+      ggplot() +  
+      geom_polygon(aes(x = long, 
+                       y = lat,
+                       group = group, 
+                       fill = valor),  
+                   color = "white", 
+                   size = .001) + 
+      facet_wrap(~ Descripción) + 
+      theme(legend.position = "bottom") + 
+      guides(fill = guide_legend(direction = "vertical")) +
+      coord_equal()
+  })
+  
+  output$map_problemas_gg <- renderPlot({
+    state_selec <- StateSelec()
+    
+    mxmunicipio.map %>% 
+      as.tibble() %>% 
+      mutate(state_code = parse_number(str_sub(id, 1, 2)), 
+             mun_code = parse_number(str_sub(id, 3, 5)) ) %>% 
+      full_join(tab_union_indicadores %>% 
+                  filter(Tema == "problemas") %>% 
+                  filter(Descripción == input$problemas.input),
+                by = c("state_code", "mun_code")) %>% 
+      filter(state_code %in% state_selec) %>% 
+      ggplot() +  
+      geom_polygon(aes(x = long, 
+                       y = lat,
+                       group = group, 
+                       fill = valor),  
+                   color = "white", 
+                   size = .001) + 
+      facet_wrap(~ Descripción) + 
+      theme(legend.position = "bottom") + 
+      guides(fill = guide_legend(direction = "vertical")) +
+      coord_equal()
+  })
+ 
+  
+  
+   output$map_percinseg_gg <- renderPlot({
+    state_selec <- StateSelec()
+    
+    mxmunicipio.map %>% 
+      as.tibble() %>% 
+      mutate(state_code = parse_number(str_sub(id, 1, 2)), 
+             mun_code = parse_number(str_sub(id, 3, 5)) ) %>% 
+      full_join(tab_union_indicadores %>% 
+                  filter(Tema == "inseguridad") ,
+                by = c("state_code", "mun_code")) %>% 
+      filter(state_code %in% state_selec) %>% 
+      ggplot() +  
+      geom_polygon(aes(x = long, 
+                       y = lat,
+                       group = group, 
+                       fill = valor),  
+                   color = "white", 
+                   size = .001) + 
+      facet_wrap(~ Descripción) + 
+      theme(legend.position = "bottom") + 
+      guides(fill = guide_legend(direction = "vertical")) +
       coord_equal()
   })
   
--- reports/shn_eda_indicadores/ui.R
+++ reports/shn_eda_indicadores/ui.R
@@ -15,67 +15,156 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("EDA: Indicadores"),
   
-  wellPanel(fixedRow(
-    column(5,
-           selectInput(inputId = "indicador.input", 
-                       label = "Indicador",
-                       selected = "",
-                       choices =c(
-                         "percepcion inseguridad" = "percepcion inseguridad",
-                         "violacion" = "violacion",
-                         "homicidios" = "homicidios",
-                         "secuestro" = "secuestro",
-                         "robo" = "robo",
-                         "vicper_amenazas" = "vicper_amenazas",
-                         "vicper_extorsión" = "vicper_extorsión",
-                         "vicper_fraude_bancario" = "vicper_fraude_bancario",
-                         "vicper_fraude_consumidor" = "vicper_fraude_consumidor",
-                         "vicper_hostigamiento_sexual" = "vicper_hostigamiento_sexual",
-                         "vicper_lesiones_físicas" = "vicper_lesiones_físicas",
-                         "vicper_otros" = "vicper_otros",
-                         "desaparicion forzada" = "desaparicion forzada"))
-      ),
-    column(5, selectInput(inputId = "estado.input", 
-                label = "Estado",
-                selected = "all",
-                choices = c("0 México" = "all", 
-                  "1 AGUASCALIENTES" = 1,
-                  "2 BAJA CALIFORNIA" = 2,
-                  "3 BAJA CALIFORNIA SUR" = 3,
-                  "4 CAMPECHE" = 4,
-                  "5 COAHUILA" = 5,
-                  "6 COLIMA" = 6,
-                  "7 CHIAPAS" = 7,
-                  "8 CHIHUAHUA" = 8,
-                  "9 CIUDAD DE MEXICO" = 9,
-                  "10 DURANGO" = 10, 
-                  "11 GUANAJUATO" = 11, 
-                  "12 GUERRERO" = 12, 
-                  "13 HIDALGO" = 13, 
-                  "14 JALISCO" = 14, 
-                  "15 MEXICO" = 15, 
-                  "16 MICHOACAN" = 16, 
-                  "17 MORELOS" = 17, 
-                  "18 NAYARIT" = 18, 
-                  "19 NUEVO LEON" = 19, 
-                  "20 OAXACA" = 20, 
-                  "21 PUEBLA" = 21, 
-                  "22 QUERETARO" = 22, 
-                  "23 QUINTANA ROO" = 23, 
-                  "24 SAN LUIS POTOSI" = 24, 
-                  "25 SINALOA" = 25, 
-                  "26 SONORA" = 26, 
-                  "27 TABASCO" = 27, 
-                  "28 TAMAULIPAS" = 28,
-                  "29 TLAXCALA" = 29,
-                  "30 VERACRUZ" = 30,
-                  "31 YUCATAN" = 31,
-                  "32 ZACATECAS" = 32)))
-  )),
+  br(),
   
-  # Show a plot of the generated distribution
-  mainPanel(
-     plotOutput("map_gg", width = "1000px", height = "600px")
-  )
+  wellPanel(fixedRow(column(5, selectInput(inputId = "estado.input", 
+                        label = "Estado",
+                        selected = "all",
+                        choices = c("0 México" = "all", 
+                                    "1 AGUASCALIENTES" = 1,
+                                    "2 BAJA CALIFORNIA" = 2,
+                                    "3 BAJA CALIFORNIA SUR" = 3,
+                                    "4 CAMPECHE" = 4,
+                                    "5 COAHUILA" = 5,
+                                    "6 COLIMA" = 6,
+                                    "7 CHIAPAS" = 7,
+                                    "8 CHIHUAHUA" = 8,
+                                    "9 CIUDAD DE MEXICO" = 9,
+                                    "10 DURANGO" = 10, 
+                                    "11 GUANAJUATO" = 11, 
+                                    "12 GUERRERO" = 12, 
+                                    "13 HIDALGO" = 13, 
+                                    "14 JALISCO" = 14, 
+                                    "15 MEXICO" = 15, 
+                                    "16 MICHOACAN" = 16, 
+                                    "17 MORELOS" = 17, 
+                                    "18 NAYARIT" = 18, 
+                                    "19 NUEVO LEON" = 19, 
+                                    "20 OAXACA" = 20, 
+                                    "21 PUEBLA" = 21, 
+                                    "22 QUERETARO" = 22, 
+                                    "23 QUINTANA ROO" = 23, 
+                                    "24 SAN LUIS POTOSI" = 24, 
+                                    "25 SINALOA" = 25, 
+                                    "26 SONORA" = 26, 
+                                    "27 TABASCO" = 27, 
+                                    "28 TAMAULIPAS" = 28,
+                                    "29 TLAXCALA" = 29,
+                                    "30 VERACRUZ" = 30,
+                                    "31 YUCATAN" = 31,
+                                    "32 ZACATECAS" = 32)))
+                     )),
   
+  tabsetPanel(
+    tabPanel("Indicador", 
+             wellPanel(fixedRow(
+               column(5,
+                      selectInput(inputId = "indicador.input", 
+                                  label = "Indicador",
+                                  selected = "",
+                                  choices =c(
+                                    "violacion sexual" = "violación sexual",
+                                    "homicidio" = "homicidio",
+                                    "secuestro" = "secuestro",
+                                    "desaparicion forzada" = "desparición",
+                                    "robo" = "robo",
+                                    "amenazas" = "amenazas",
+                                    "extorsión" = "extorsión",
+                                    "fraude bancario" = "fraude bancario",
+                                    "fraude consumidor" = "fraude consumidor",
+                                    "abuso sexual" = "abuso sexual",
+                                    "agresión física" = "agresión física",
+                                    "otros" = "otros"))
+               )
+             )),
+             
+             # Show a plot of the generated distribution
+             mainPanel(
+               plotOutput("map_inds_gg", width = "1000px", height = "700px")
+             )
+    ),  # indicador panel
+    
+    tabPanel("Importancia",
+             wellPanel(fixedRow(
+               column(5,
+                      selectInput(inputId = "importancia.input", 
+                                  label = "Tema de importancia",
+                                  selected = "",
+                                  choices =c("Agua",
+                                    "Corrupción",
+                                    "Desastres",
+                                    "Desempleo",
+                                    "Educación",
+                                    "Impunidad",
+                                    "Inseguridad",
+                                    "Narco",
+                                    "Pobreza",
+                                    "Precios",
+                                    "Salud",
+                                    "Otro",
+                                    "No sabe"
+                                    ))
+               )
+             )),
+             mainPanel(
+               plotOutput("map_importancia_gg", width = "1000px", height = "700px")
+             )
+    ),   # importancia panel
+    
+    tabPanel("Percepción",
+             wellPanel(fixedRow(
+               column(5,
+                      selectInput(inputId = "percepcion.input", 
+                                  label = "Tema de percepción",
+                                  selected = "",
+                                  choices =c("alcohol en la calle",
+                                             "consumo drogas",
+                                             "disparos",
+                                             "extorsiones",
+                                             "homicidios",
+                                             "invasión de predios",
+                                             "ninguna",
+                                             "no sabe",
+                                             "pandillerismo",
+                                             "policia vs ciudadanos",
+                                             "prostitución",
+                                             "riñas entre vecinos",
+                                             "robos asaltos",
+                                             "secuestros",
+                                             "venta droga",
+                                             "venta ilegal alcohol",
+                                             "venta prods pirata"))
+               )
+             )),
+             mainPanel(
+               plotOutput("map_percepcion_gg", width = "1000px", height = "700px")
+             )
+    ),   # percepción panel
+    
+    tabPanel("Problemas",
+             wellPanel(fixedRow(
+               column(5,
+                      selectInput(inputId = "problemas.input", 
+                                  label = "Tema de problema",
+                                  selected = "",
+                                  choices =c("baches o fugas de agua",
+                                             "delincuencia cerca escuelas",
+                                             "falta agua",
+                                             "falta alumbrado",
+                                             "pandillerismo violento",
+                                             "robos"))
+               )
+             )),
+             mainPanel(
+               plotOutput("map_problemas_gg", width = "1000px", height = "700px")
+             )
+    ),   # problema panel
+    
+    tabPanel("Percepción Inseguridad",
+             mainPanel(
+               plotOutput("map_percinseg_gg", width = "1000px", height = "700px")
+             )
+    )   # problema panel
+    
+  ) # tabset panel
 ))
