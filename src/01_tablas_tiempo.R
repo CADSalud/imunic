4ae6a65141778c6d6ebd39060ad33fa9b4203e77
Songeo <sonia.gmcl@gmail.com>
2018-03-06 23:15:14 -0600

    tablas de envipe para shiny

--- src/01_tablas_tiempo.R
+++ src/01_tablas_tiempo.R
@@ -3,42 +3,63 @@ library(ProjectTemplate)
 load.project()
 
 
+# Tablas resúmen ----
+load("cache/tab_pvic1.RData")
+load("cache/tab_pvic2.RData")
 
-tab_vic_hogar <- envipe_vic2_year %>% 
-  dplyr::select(CONTROL, VIV_SEL,
-                ID_PER, ID_VIV, ID_HOG, 
-                UPM, HOGAR, 
-                CVE_ENT, CVE_MUN,
-                starts_with("FAC_"),
-                year_file, 
-                AP6_10_1, AP6_10_2, 
-                AP6_15_1, AP6_15_2, 
-                AP6_20_1, AP6_20_2) %>% 
-  gather(codigo, val, starts_with("AP6_")) %>% 
-  separate(codigo, into = c("cod", "delito", "preg"), sep = "_") %>% 
-  mutate(preg = factor(preg, 1:2, c("integrante_bin", "cuantos"))) %>% 
-  spread(preg, val) %>% 
-  mutate(FAC_HOG = parse_number(FAC_HOG),
-         cuantos = parse_number(cuantos),
-         integrante_bin = parse_number(integrante_bin)) %>% 
-  filter(integrante_bin == 1) %>% 
-  group_by(year_file, CVE_ENT, CVE_MUN, delito) %>% 
-  summarise(hogar_n = sum(integrante_bin), 
-            cuantos_mean = median(cuantos), 
-            hogar_fach = sum(integrante_bin*FAC_HOG)) %>% 
-  ungroup()
-tab_vic_hogar
 
-tab_vic_hogar %>% 
-  mutate(delito = factor(delito, 
-                         c(10, 15, 20), 
-                         c("secuestro", "desap_forzada", "homicidio"))) %>% 
-  group_by(year_file, CVE_ENT, CVE_MUN, delito) %>% 
-  summarise(cnt_vichgrp = sum(hogar_fach)*mean(cuantos_mean)) %>% 
+# datos victimización ----
+tab_1 <- tab_pvic2 %>% 
+  group_by(ENT, MUN, Tema, Descripción, value) %>% 
+  summarise(n = sum(n), 
+            n_ele = sum(n_ele),
+            n_hog = sum(n_hog),
+            n_viv = sum(n_viv)) %>% 
   ungroup() %>% 
-  mutate(CVE_ENT = parse_number(CVE_ENT), 
-         CVE_MUN = parse_number(CVE_MUN)) %>% 
-  rename(state_code = CVE_ENT, 
-         mun_code = CVE_MUN) %>% 
-  mutate(delito = paste0("vichgr_", delito)) %>% 
-  spread(delito, cnt_vichgrp)
+  filter(!str_detect(Descripción, "num"),
+         value == 1) %>% 
+  rename(state_code = ENT, mun_code = MUN) %>% 
+  mutate(nfac = ifelse(Tema == "víctima hogar", 
+                       n_hog, 
+                       n_ele)) %>% 
+  dplyr::select(state_code, mun_code,
+                Tema, Descripción, 
+                valor = nfac) %>% 
+  mutate(Descripción = str_trim(str_replace(tolower(Descripción), "sí", "")))
+
+
+# datos victimización ----
+tab_2 <- tab_pvic1 %>% 
+  filter(Tema == "inseguridad") %>% 
+  group_by(ENT, MUN, Tema, Descripción, value) %>% 
+  summarise(n = sum(n), 
+            nfac = sum(n_ele)) %>% 
+  group_by(ENT, MUN, Tema, Descripción ) %>% 
+  mutate(prop = nfac/sum(nfac)) %>% 
+  ungroup() %>% 
+  filter(value == 2) %>% 
+  rename(state_code = ENT, mun_code = MUN) %>% 
+  dplyr::select(state_code, mun_code,
+                Tema, Descripción, valor = prop) 
+
+
+tab_3 <- tab_pvic1 %>% 
+  filter(Tema %in% c("importancia", 'percepción', "problemas")) %>% 
+  group_by(ENT, MUN, Tema, Descripción, value) %>% 
+  summarise(n = sum(n), 
+            n_ele = sum(n_ele)) %>% 
+  group_by(ENT, MUN, Tema, Descripción) %>% 
+  mutate(prop = n_ele/sum(n_ele)) %>% 
+  ungroup() %>% 
+  filter(value == 1) %>% 
+  rename(state_code = ENT, mun_code = MUN) %>% 
+  dplyr::select(state_code, mun_code,
+                Tema, Descripción, valor = prop)
+
+
+
+# Union de tabla ----
+tab_1 %>% 
+  bind_rows(tab_2) %>% 
+  bind_rows(tab_3)
+
