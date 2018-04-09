c066d3ecb6b3a659f48ca6a41ba901c96157256e
Songeo <sonia.gmcl@gmail.com>
2018-04-09 12:08:35 -0500

    cambio de nombre de script de funciones

--- reports/src_mun/funciones_datos.R
+++ reports/src_mun/funciones_datos.R
@@ -25,6 +25,19 @@ tablas_load_fun <- function(municipios_selec, state_code_selec){
            state_code == state_code_selec)
   
   
+  # Damus 
+  load("cache/tab_damus_rmd.RData")
+  tab_damus <- tab_damus_rmd %>% 
+    right_join(tab_estmun_selec, 
+               by = c("state_code", "mun_code"))
+  
+  
+  # Enigh 
+  load("cache/tab_enigh2016_rmd.RData")
+  tab_enigh <- tab_enigh2016_rmd %>% 
+    right_join(tab_estmun_selec, 
+               by = c("state_code", "mun_code"))
+  
   # Envipe
   load("cache/tab_union_indicadores.RData")
   tab_envipe <- tab_union_indicadores %>% 
@@ -50,8 +63,13 @@ tablas_load_fun <- function(municipios_selec, state_code_selec){
   tab_ingreso_ent <- pob_trabajaingreso_ent %>% 
     filter(parse_number(ENT) == tab_estmun_selec$state_code)
   
+  
+ 
+  
   tablas <- list(tab_envipe = tab_envipe, 
                  tab_interc = tab_interc, 
+                 tab_enigh = tab_enigh, 
+                 tab_damus = tab_damus, 
                  tab_estatal = tab_estatal, 
                  entidad = tab_estmun_selec, 
                  tab_ingreso_ent = tab_ingreso_ent)
